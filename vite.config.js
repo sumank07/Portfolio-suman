9cebb2a3e0e3ef79bf2b0e1eaf0aaf15730817a9
suman katuwal <suman.katuwal@aadimcollege.edu.np>
2025-03-04 17:30:56 +0545

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // Ensures relative paths for assets
+  base: ' http://localhost:3001/sumank07/portfolio-suman.git ',  // Ensures relative paths for assets
 });
