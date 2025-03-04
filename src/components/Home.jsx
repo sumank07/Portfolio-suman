26f4a9197192bb0cae8e6b1fa40b83acac26277e
suman katuwal <suman.katuwal@aadimcollege.edu.np>
2025-03-04 15:30:00 +0545

    commit it

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
         transition-transform duration-300 hover:scale-105"
         />
        <h1 className="text-2xl">
-          I am Suman Katuwal From sankhuwasava.
+          I am Suman Katuwal From sankhuwasava. herohonda chalauxu moo
         </h1>
       </div>
     </section>
