cb850bd8010277fcc0daa06ca16daa14950f480e
suman katuwal <suman.katuwal@aadimcollege.edu.np>
2025-03-05 15:02:24 +0545

    Deploying to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  base: ' http://localhost:3001/sumank07/portfolio-suman.git ',  // Ensures relative paths for assets
+  base: "/portfolio-suman/", // Make sure this matches your repository name
 });
